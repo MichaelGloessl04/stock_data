ca7bd0909d6845767c9221ff9a5c8ec06de2a0c6
MichaelGloessl04 <gloess190117@gmail.com>
2024-01-09 19:09:29 +0100

    updated .gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stock_importer',
+    version='0.0.1',
+    description='Import stock data and visualize it.',
+    author='Michael Glössl',
+
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'pytest',
+        'pandas'
+    ]
+)
