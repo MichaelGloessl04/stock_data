946169db424aa9a5dba1d688882413cee5c73c29
MichaelGloessl04 <gloess190117@sr.htlweiz.at>
2024-01-10 11:19:35 +0100

    fixed issues with pytest discovery errors in src folder

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from . import stock_importer
+from . import visualization
+
+exports = [stock_importer, visualization]
--- tests/test_stock_importer.py
+++ tests/test_stock_importer.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from src.stock_importer import StockImporter
 
-
 def test_init():
     importer = StockImporter()
 
